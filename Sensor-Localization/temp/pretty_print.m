89a73d06e400f194582c5ed16ccd0f16d2c3e5d5
unknown <itj3@RODIA.cefns.nau.edu>
2015-03-12 14:11:37 -0700

    added extra variable, test_mode, to pretty_print. This is a boolean value. If test_mode = false the printout will exclude the unknown nodes acctual room locations

--- Sensor-Localization/temp/pretty_print.m
+++ Sensor-Localization/temp/pretty_print.m
@@ -1,8 +1,9 @@
 % data = output from Prior_Distribution
 % nodes_data = output from load_known_nodes
 % unknown_nodes_number = the number of unknown nodes we are checking
+% test_mode = boolean, if we are testing test_mode = true
 
-function pretty_print(data,nodes_data,unknown_nodes_number)
+function pretty_print(data, nodes_data, unknown_nodes_number, test_mode)
     count = 2;
     node1 = mat2str(cell2mat(data(1,1)));
     for i=1:unknown_nodes_number
@@ -44,6 +45,11 @@ function pretty_print(data,nodes_data,unknown_nodes_number)
             first_rooms = temp_rooms;
             node2 = mat2str(cell2mat(data(count,1)));
             disp('-----------------------');
+            if test_mode
+                disp(strcat('node:',node1,':', num2str(cell2mat(nodes_data((str2double(node1)+1),2)))));
+            else
+                disp(strcat('node:',node1));
+            end
             disp(strcat('node:',node1,':', num2str(cell2mat(nodes_data((str2double(node1)+1),2)))));
             disp(strcat(first_rooms(1,:),':',num2str(first_list(1))));
             disp(strcat(first_rooms(2,:),':',num2str(first_list(2))));
@@ -52,7 +58,11 @@ function pretty_print(data,nodes_data,unknown_nodes_number)
         elseif i==2
             second_list = temp_list;
             second_rooms = temp_rooms;
-            disp(strcat('node:',node2,':', num2str(cell2mat(nodes_data((str2double(node2)+1),2)))));
+            if test_mode
+                disp(strcat('node:',node2,':', num2str(cell2mat(nodes_data((str2double(node2)+1),2)))));
+            else
+                disp(strcat('node:',node2));
+            end
             disp(strcat(second_rooms(1,:),':',num2str(second_list(1))));
             disp(strcat(second_rooms(2,:),':',num2str(second_list(2))));
             disp(strcat(second_rooms(3,:),':',num2str(second_list(3))));
@@ -62,7 +72,11 @@ function pretty_print(data,nodes_data,unknown_nodes_number)
         else
             third_list = temp_list;
             third_rooms = temp_rooms;
-            disp(strcat('node:',node3,':', num2str(cell2mat(nodes_data((str2double(node3)+1),2)))));
+            if test_mode
+                disp(strcat('node:',node3,':', num2str(cell2mat(nodes_data((str2double(node3)+1),2)))));
+            else
+                disp(strcat('node:',node3));
+            end
             disp(strcat(third_rooms(1,:),':',num2str(third_list(1))));
             disp(strcat(third_rooms(2,:),':',num2str(third_list(2))));
             disp(strcat(third_rooms(3,:),':',num2str(third_list(3))));
